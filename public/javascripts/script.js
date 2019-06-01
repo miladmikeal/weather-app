cc33c15027d5e33d1c3a1f6976f99f76b27825be
Milad Mikeal <miladmikeal@gmail.com>
2019-05-31 18:59:45 -0700

    Add weather scripts

---
+++ public/javascripts/script.js
@@ -0,0 +1,31 @@
+const weatherForm = document.querySelector('form');
+
+const search = document.querySelector('input');
+
+const messageOne = document.querySelector('#message-1');
+const addrress = document.querySelector('#address');
+const temperature = document.querySelector('#temperature');
+const summary = document.querySelector('#summary');
+
+messageOne.textContent = '';
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const searchValue = search.value;
+
+  fetch(`http://localhost:3000/weather?address=${searchValue}`)
+    .then(response => {
+      response.json()
+        .then(data => {
+          if (data.error) {
+            messageOne.textContent = data.error;
+          } else {
+            address.textContent = data.location;
+            temperature.textContent = `It is currently ${data.temperature} degrees.`;
+            summary.textContent = data.summary;
+          }
+        });
+    });
+});
+
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var geocode = require('../utils/geocode');
+var forecast = require('../utils/forecast');
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+  res.render('index');
+});
+
+router.get('/weather', (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: 'You must provide an address'
+    });
+  }
+  geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+    if (error) {
+      return res.send({
+        error
+      });
+    }
+    forecast(latitude, longitude, (error, { temperature, summary }) => {
+      if (error) {
+        return res.send({
+          error
+        });
+      }
+      res.send({
+        location,
+        temperature,
+        summary
+      });
+    });
+  });
 });
 
 module.exports = router;
